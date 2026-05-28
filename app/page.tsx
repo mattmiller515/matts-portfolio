45442ae34ba016a912a17d1afdca4f562d3cb8bd
Matt <mattmiller515@gmail.com>
2026-05-28 14:36:24 -0500

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,23 @@ import { ContentCard } from "@/components/base/ContentCard";
 import { AboutMe } from "@/components/widgets/AboutMe";
 import { Header } from "@/components/widgets/Header";
 import { Projects } from "@/components/widgets/Projects";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
-    <div className="p-4">
-      <Header />
-      <div className="mt-12 flex flex-col gap-4 items-center max-w-7xl mx-auto">
-        <ContentCard>
-          <AboutMe />
-        </ContentCard>
-        <ContentCard>
-          <Projects />
-        </ContentCard>
+    <>
+      <Analytics />
+      <div className="p-4">
+        <Header />
+        <div className="mt-12 flex flex-col gap-4 items-center max-w-7xl mx-auto">
+          <ContentCard>
+            <AboutMe />
+          </ContentCard>
+          <ContentCard>
+            <Projects />
+          </ContentCard>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
